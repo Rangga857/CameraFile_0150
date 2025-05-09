2e8b6bde3025de430fb5e43e167ad91ccb58c720
Rangga857 <azharrangga28@gmail.com>
2025-05-09 15:00:25 +0700

    initState: mode immersive & inisialisasi kamera

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
@@ -11,6 +12,20 @@ class CameraPage extends StatefulWidget {
 class _CameraPageState extends State<CameraPage> {
   late List<CameraDescription> cameras;
   CameraController? controller;
+  int selectedCameraIdx = 0;
+  FlashMode flashMode = FlashMode.off;
+  double zoomLevel = 1.0;
+  double maxZoom = 1.0;
+  double minZoom = 1.0;
+  bool isZoomSuported = false;
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    _initializeCamera();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
