22c8485963656915cf52d824599eaf5de7853921
Rangga857 <azharrangga28@gmail.com>
2025-05-15 18:29:01 +0700

    Implementasi _onInit untuk inisialisasi kamera dan setup controller awal

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -18,4 +18,13 @@ class CameraBloc extends Bloc<CameraEvent, CameraState> {
     on<ClearSnackbar>(_onClearSnackbar);
     on<RequestPermissions>(_onRequestPermissions);
   }
+
+  Future<void> _onInit(
+    InitializeCamera event,
+    Emitter<CameraState> emit,
+  ) async {
+    _cameras = await availableCameras();
+    await _setupController(emit, 0);
+  }
+  
 }
