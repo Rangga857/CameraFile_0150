bfb4f4e1cd8f02f4e98e3e04d19f5611802347e8
Rangga857 <azharrangga28@gmail.com>
2025-05-15 18:15:19 +0700

    create event untuk mengambil gambar

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 sealed class CameraEvent {}
 
 final class InitializeCamera extends CameraEvent {}
@@ -5,3 +7,8 @@ final class InitializeCamera extends CameraEvent {}
 final class SwitchCamera extends CameraEvent {}
 
 final class ToggleFlash extends CameraEvent {}
+
+final class TakePicture extends CameraEvent {
+  final void Function(File imageFile) onPictureTaken;
+  TakePicture(this.onPictureTaken);
+}
