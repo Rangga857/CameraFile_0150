5bd82beabb7ba0cb3bffe5fed172558c04d45141
Rangga857 <azharrangga28@gmail.com>
2025-05-15 17:14:06 +0700

    add button untuk ambil gambar dan pilih galeri

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -48,6 +48,32 @@ class _FullPageState extends State<FullPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Beranda')),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton.icon(
+                    icon: const Icon(Icons.camera),
+                    label: const Text('Ambil Foto'),
+                    onPressed: _takePicture,
+                  ),
+                ),
+                ElevatedButton.icon(
+                  icon: const Icon(Icons.folder),
+                  label: const Text('Pilih dari Galeri'),
+                  onPressed: _pickFromGallery,
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+          ]
+        )
+      ),
+    );
   }
 }
