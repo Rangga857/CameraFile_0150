26e3710b41e53573df25c44ea5dc594f7f489c9b
Rangga857 <azharrangga28@gmail.com>
2025-05-15 19:52:14 +0700

    Tambah BlocConsumer untuk snackbar dan render image jika ada

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
+import 'package:camera_app/bloc/camera_bloc.dart';
+import 'package:camera_app/bloc/camera_event.dart';
+import 'package:camera_app/bloc/camera_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,6 +13,25 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Beranda'),),
+      body: SafeArea(
+        child: BlocConsumer<CameraBloc, CameraState>(
+          listener: (context, state){
+            if (state is CameraReady && state.snackbarMessage != null){
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(state.snackbarMessage!)),
+              );
+              context.read<CameraBloc>().add(ClearSnackbar());
+            }
+          },
+          builder: (context, state){
+            final File? imageFile =
+              state is CameraReady ? state.imageFile : null;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+            );
+          },
+        )
+      ),
     );
   }
 }
